81c75d7e8b99d30c489b7d4e2ff30029d4b4e9d5
yangxue <email@yangxue.com>
2019-07-15 08:26:58 +0800

    03

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react'
 import {Button, message} from 'antd'
+import {BrowserRouter,HashRouter,Switch,Route,Redirect} from 'react-router-dom'
+ import Login from './pages/login/login' 
+ import Admin from './pages/admin/admin'
 
-
-export default class App extends Component {
+ export default class App extends Component {
 
     
     handleClick = () => {
@@ -12,7 +14,13 @@ export default class App extends Component {
     render() {
 
       return (
-        <Button type='primary' onClick={this.handleClick}>学习</Button>
+       <BrowserRouter>
+         <Switch>
+           <Route path="/login"  component={Login}></Route>
+           {/* <Route path="/"  Component={Admin}></Route> */}
+           <Redirect to="/login"></Redirect>
+         </Switch>
+       </BrowserRouter>
       )
     }
   }
